3199822212d5f2dd5c1fc5b573149cbee28121e2
Amer Varajic <varajicamer@gmail.com>
2023-04-25 15:55:41 +0200

    added restaurants to the list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -14,22 +14,10 @@ import PlaceDetails from "../PlaceDetails/PlaceDetails";
 import useStyles from "./styles";
 import { useState } from "react";
 
-const List = () => {
+const List = ({ places }) => {
   const classes = useStyles();
   const [type, setType] = useState("restaurants");
   const [rating, setRating] = useState("");
-
-  const places = [
-    { name: "Cool place" },
-    { name: "Best Beer" },
-    { name: "Best Steak" },
-    { name: "Cool place" },
-    { name: "Best Beer" },
-    { name: "Best Steak" },
-    { name: "Cool place" },
-    { name: "Best Beer" },
-    { name: "Best Steak" },
-  ];
 
   return (
     <div className={classes.container}>
